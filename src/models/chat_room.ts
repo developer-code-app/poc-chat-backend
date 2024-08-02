3463666153e6c25815d18c315eb802588f6b273b
Takdanai Ittipak <takdanai@code-app.com>
2024-08-02 17:51:54 +0700

    update model

--- src/models/chat_room.ts
+++ src/models/chat_room.ts
@@ -1,55 +1,23 @@
-import { ChatRoomMember } from "./chat_room_member";
-import { Message } from "./message";
-
-enum SyncState {
-  SYNCED = "SYNCED",
-  SYNCING = "SYNCING",
-  CORRUPTED = "CORRUPTED",
-}
-
-class ChatRoomSyncState {
-  readonly messageSyncState: SyncState;
-  readonly chatRoomManagementSyncState: SyncState;
-
-  constructor(
-    messageSyncState: SyncState,
-    chatRoomManagementSyncState: SyncState
-  ) {
-    this.messageSyncState = messageSyncState;
-    this.chatRoomManagementSyncState = chatRoomManagementSyncState;
-  }
-}
+import { ReadMessageEvent } from "./event/read_event";
+import { RoomManagementEvent } from "./event/room_management_event";
 
 class ChatRoom {
   readonly id: number;
-  readonly name: string;
-  readonly thumbnailUrl: string;
-  readonly members: ChatRoomMember[];
-  readonly confirmedMessages: Message[];
-  readonly failedMessages: Message[];
-  readonly sendingMessages: Message[];
+  readonly roomManagementEvents: RoomManagementEvent[];
+  readonly messageEvents: MessageEvent[];
+  readonly readEvents: ReadMessageEvent[];
 
   constructor(
     id: number,
-    name: string,
-    thumbnailUrl: string,
-    members: ChatRoomMember[],
-    confirmedMessages: Message[],
-    failedMessages: Message[],
-    sendingMessages: Message[]
+    roomManagementEvents: RoomManagementEvent[],
+    messageEvents: MessageEvent[],
+    readEvents: ReadMessageEvent[]
   ) {
     this.id = id;
-    this.name = name;
-    this.thumbnailUrl = thumbnailUrl;
-    this.members = members;
-    this.confirmedMessages = confirmedMessages;
-    this.failedMessages = failedMessages;
-    this.sendingMessages = sendingMessages;
-  }
-
-  static createChatRoom(): ChatRoom {
-    return new ChatRoom(0, "", "", [], [], [], []);
+    this.roomManagementEvents = roomManagementEvents;
+    this.messageEvents = messageEvents;
+    this.readEvents = readEvents;
   }
 }
 
-export { ChatRoom, ChatRoomSyncState, SyncState };
+export { ChatRoom };
